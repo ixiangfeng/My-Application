30bc464133cd15ab9bcdd1522f2cdeacac0c1123
cxh <576268140@qq.com>
2015-07-07 22:38:14 +0800

    Initial commit

---
+++ myapplication2/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.administrator.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+
+public class MainActivity extends AppCompatActivity {
+    private Button setContentViewv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentViewv = new Button(this);
+        TextView tv = new TextView(this);
+        Gson gson = new Gson();
+        Gson gson2 = new Gson();
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
